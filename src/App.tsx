73531e63e47c879c8447189ee1a0dd88ba9c8a2c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-12 12:44:08 +0200

    Add remove to do

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NewTodo from './components/NewTodo';
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
 
@@ -13,10 +14,16 @@ function App() {
     });
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
